4ba864f3aa579acba0422a7be836f11bf06c7f23
Jonghoon Seo <jonghoon.seo@gmail.com>
2014-10-28 17:03:58 +0900

    Sender 완료 - FTP 전송 완료 - FTP 디렉토리 생성 완료 - FTP 업로드 후 OSC 전송 완료

--- src/kr/dragshare/dragshare_client/SenderFragment.java
+++ src/kr/dragshare/dragshare_client/SenderFragment.java
@@ -1,19 +1,21 @@
 package kr.dragshare.dragshare_client;
 
-import java.io.IOException;
 import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
+import kr.dragshare.dragshare_client.networkManager.FTPNetworkManager;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.illposed.osc.OSCMessage;
 import com.illposed.osc.OSCPortOut;
@@ -22,6 +24,68 @@ public class SenderFragment extends Fragment {
 	
 	private View rootView;
 	
+	//=======================================================================================
+	//===		FTP Asynchronous Task 
+	public class FTPTask extends AsyncTask<String, Integer, Void> {
+		FTPNetworkManager network;
+		
+		final String 	host = "165.132.107.90";
+		final int		port = 21;
+		final String 	id	 = "msl";
+		final String	pw	 = "0";
+		
+		String	targetPath = "/";
+		
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			
+			network = new FTPNetworkManager();
+			
+			// Progress Update
+			//------------------------
+//			network.setFTPTask(this);						// to process upload progress, transfer this instance to FTPNetworkManager 
+		}
+		
+		@Override
+		protected Void doInBackground(String... params) {
+			network.initialize(host, port, id, pw);
+			
+			targetPath = "/" + Util.getUniqueDirectory(rootView) + "/";
+
+			if(!network.send(params[0], targetPath + Util.getFileName(params[0]))){
+				Log.e("NetworkManager", "Sending Failed");
+			}
+			
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// Notify done
+			Toast.makeText(rootView.getContext(), "FTP Upload Done", Toast.LENGTH_SHORT).show();
+
+			OSCTask osc = new OSCTask();
+			osc.execute(targetPath);
+			
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+//        	Toast.makeText(getApplicationContext(), "transferred: " + ftp.percent + "%", Toast.LENGTH_SHORT).show();
+			super.onProgressUpdate(values);
+		}	
+	}
+	//=======================================================================================
+	
+	
+	
+	
+	
+	//=======================================================================================
+	//===		OSC Asynchronous Task 
 	public class OSCTask extends AsyncTask<String, Void, Void> {		
 		@Override
 		protected Void doInBackground(String... params) {
@@ -29,7 +93,7 @@ public class SenderFragment extends Fragment {
 			//----------------------------------------------------
 			try {
 				OSCPortOut sender = new OSCPortOut(InetAddress.getByName("165.132.107.90"), 3746);
-				OSCMessage msg = new OSCMessage("/dragshare/sender", new Object[]{params[0], params[1]});
+				OSCMessage msg = new OSCMessage("/dragshare/sender", new Object[]{params[0], Util.getCurrentTimeString()});
 
 				sender.send(msg);
 			} catch (Exception e) {
@@ -40,10 +104,11 @@ public class SenderFragment extends Fragment {
 		
 		protected void onPostExecute(Void result) {
 			EditText log = (EditText)rootView.findViewById(R.id.editText1);
-			log.setText("OSC done");
+			log.setText("OSC sent");
 			super.onPostExecute(result);
 		}
 	}
+	//=======================================================================================
 	
 	public SenderFragment() {
 	}
@@ -51,8 +116,7 @@ public class SenderFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		rootView = inflater.inflate(R.layout.fragment_sender, container,
-				false);
+		rootView = inflater.inflate(R.layout.fragment_sender, container, false);
 		
 		Button button = (Button)rootView.findViewById(R.id.button1);
 		button.setOnClickListener(new OnClickListener() {
@@ -60,13 +124,50 @@ public class SenderFragment extends Fragment {
 			@Override
 			public void onClick(View v) {
 				// 1. FTP 전송
-				// 2. OSC 전송
-				new OSCTask().execute(Util.getDeviceIdentifier(), Util.getCurrentDateString());
+	        	// Initialize FTP Task instance
+	        	FTPTask ftp = new FTPTask();
+	        	
+	        	// Get the file name of last picture 
+	        	final String lastPicture = getLastPictureName();
+
+	        	// Notify
+	        	Toast.makeText(rootView.getContext(), "Image upload: " + lastPicture, Toast.LENGTH_LONG).show();
+	        	
+	        	// Go and upload
+	        	ftp.execute(lastPicture);
+				
+//				// 2. OSC 전송
+//				new OSCTask().execute(Util.getDeviceIdentifier(rootView), Util.getCurrentDateString());
 			}
 		});
 		
 		return rootView;
 	}
 
+	private final String getLastPictureName() {
+		// Find the last picture
+		String[] projection = new String[]{
+		    MediaStore.Images.ImageColumns._ID,
+		    MediaStore.Images.ImageColumns.DATA,
+		    MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+		    MediaStore.Images.ImageColumns.DATE_TAKEN,
+		    MediaStore.Images.ImageColumns.MIME_TYPE
+		    };
+		final Cursor cursor = rootView.getContext().getContentResolver()
+		        .query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, 
+		               null, MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC");
+
+		// Put it in the image view
+		if (cursor.moveToFirst()) {
+		    String imageLocation = cursor.getString(1);
+
+		    cursor.close();
+		    
+		    return imageLocation;
+		} 
+	    cursor.close();
+	    
+	    return null;
+	}
 
 }
--- src/kr/dragshare/dragshare_client/Util.java
+++ src/kr/dragshare/dragshare_client/Util.java
@@ -4,15 +4,48 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import android.provider.Settings.Secure;
+import android.view.View;
+
 public class Util {
-	public static final String getDeviceIdentifier() {
-		return "Id";
+	// DeviceID + 현재 시각
+	public static final String getUniqueDirectory(View v) {
+		String time = Long.toString(System.currentTimeMillis()); 
+		
+		return getDeviceID(v) + "_"+time;
 	}
 	
-	public static final String getCurrentDateString() {
+	// DeviceID
+	public static final String getDeviceID(View v) {
+		return Secure.getString(v.getContext().getContentResolver(), Secure.ANDROID_ID);
+	}
+	
+	// 현재 시각에 대한 문자열
+	public static final String getCurrentTimeString() {
 		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss:SSS");
 		Date dateobj = new Date();
 		
 		return df.format(dateobj);
+	}
+	
+	// Full Path에서 파일명 추출
+	public static String getFileName(String fullPath) {
+		int S = fullPath.lastIndexOf("/");
+		int M = fullPath.lastIndexOf(".");
+		int E = fullPath.length();
+		
+		String filename = fullPath.substring(S+1, M);
+		String extname = fullPath.substring(M+1, E);
+		
+		String extractFileName = filename + "." + extname;
+		return extractFileName;
+	}
+
+	public static String getDirectoryPath(String fullPath) {
+		int S = fullPath.lastIndexOf("/");
+		
+		String filename = fullPath.substring(0, S);
+		
+		return filename;
 	}
 }
